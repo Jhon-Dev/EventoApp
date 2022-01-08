fc42e0261b8e8e2e5cb69b189bbc51476c4ea8bf
Jhonatan Nuss <contato@jhonatanuss.dev>
2022-01-08 11:08:31 -0300

    Adicionando Evento controller

--- src/main/java/com/evento/controller/EventoController.java
+++ src/main/java/com/evento/controller/EventoController.java
@@ -1,2 +1,13 @@
-package com.evento.controller;public class EventoController {
+package com.evento.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+
+    @RequestMapping("/cadastrarEvento")
+    public String form(){
+        return "evento/formEvento";
+    }
 }
